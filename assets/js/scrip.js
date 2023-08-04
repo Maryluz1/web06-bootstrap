c2547212c44178a45196024401a71440682928ca
Maryluz García <ruthmaryroot@gmail.com>
2023-08-04 12:16:20 -0600

    feat:evento onclick en imagen patricio

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -3,3 +3,38 @@ console.log("Página en construcción con Patricio Estrella")
 function clickPatricio() {
     alert("La mayonesa es un instrumento")
 }
+function changeColor( elementHtml, color ) {
+    alert("Que emoción, voy a cambiar mi color")
+    
+}
+function changeColorWithPrompt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    changeTextOfUserColor( elementHtml, color);
+    changeColor( elementHtml, color );
+}
+
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+
+
+
+function resetColor(){
+        const refRedColor = document.getElementById("red-color");
+        const refGreenColor = document.getElementById("green-color");
+        const refPurpleColor = document.getElementById("purple-color");
+        const refUserColor = document.getElementById("user-color");
+    
+        // refRedColor.style.color = "black";
+        changeColor(refRedColor, 'black');
+        changeColor(refGreenColor, 'black');
+        changeColor(refPurpleColor, 'black');
+        changeColor(refUserColor, 'black');
+    
+    }
+
+    function changeName(){
+        const userName = prompt("Escribe tu nombre cholo", "Machete");
+        const refGretting = document.getElementById("gretting");
+        refGretting.innerHTML = `Hola ${userName}, la clika te respalda`;
+    }
