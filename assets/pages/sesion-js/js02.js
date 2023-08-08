9b727e76a60005ad49529851d2fcf3f67d8765d8
Maryluz García <ruthmaryroot@gmail.com>
2023-08-08 11:21:42 -0600

    ci:funciones callback y resolviendo ciclos anidados

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -59,7 +59,6 @@ else {
 console.log( saludo("Sergio"));
 console.log( saludo("David"));
 
-
 /*
  ------------ Funciones autoinvocadas -----------------------
              (selft-invoking functions)
@@ -74,9 +73,52 @@ sintaxis:
 } )( "ChatGPT" );
 
 
+/*
+ ------------ Funciones flecha -----------------------
+             (arrow functions)
+Funciones similares a las funciones expresadas pero:
+ - No requiere la palabra reservada function
+ - Si tiene una solo instrucción no requiere las llaves {}
+ - Si la instrucción es el mismo retorno, no requiere la palbra return
 
+sintaxis:
+    const nombreVariable = (parametros) => instrucción;
 
+    const nombreVariable = (parametros) => {
+        instrucción;
+        return expresión;
+    }
+*/
 
+/* const areaRectangulo = function (base, altura){
+    return base * altura;
+} */
+ 
+const areaRectangulo = (base, altura) => base * altura;
+
+console.log(`El área es: ${ areaRectangulo(3, 9)} m2.`);
+
+/*
+ ------------ Parámetros por defecto -----------------------
+             (default parameters)
+Inicializa un parámetro de la función, si no se envía el argumento cuando se invoca
+
+*/
+
+/**
+ * Calcula el área de un triángulo
+ * @param {number} base del triángulo
+ * @param {number} altura del triángulo
+ * @returns {number} base * altura / 2
+ */
+const areaTriangulo = (base = 1, altura = 1) => base * altura /2.0;
+
+console.log(`El area es: ${ areaTriangulo(4, 12)} m2.`);
+console.log(`El area es: ${ areaTriangulo()} m2.`); //0.5
+console.log(`El area es: ${ areaTriangulo(10)} m2.`); // 5
+console.log( (1000).toString()  ); // conversión base 10 -> 1000
+console.log( (1000).toString(2)  ); // conversión base 10 -> 1111101000
+console.log( (1000).toString(16)  ); // conversión base 10 -> 3e8
 
 
 /*
@@ -90,19 +132,83 @@ sintaxis:
      ...nombreParametros
 
 */
-const sumatoriaMultiplesNumeros = (a, b) => a + b;
-
-console.log(`Sumatoria de múltiples números: ${ sumatoriaMultiplesNumeros(2,5)}`); // 7
-
-
-const sumatoriaMultiplesNumeros = (a, b, ...rest) => {
+const sumatoriaMultiplesNumeros = (a = 0, b = 0, ...rest) => {
+    let suma = a + b;
     for (let i = 0; i < rest.length; i++){
-        //suma = suma + rest[i];
-        suma += rest[i];
+        suma = suma + rest[i];
       }
-      return suma;
-
-    // sumar los valores del arreglo "rest"
-
     return suma;
 }
+
+console.log(`Sumatoria de múltiples números: ${ sumatoriaMultiplesNumeros(2,5)}`); // 7
+console.log(`Múltiples números: ${ sumatoriaMultiplesNumeros(2, 5, 10, 9 )}`); // 26
+console.log(`Múltiples números: ${ sumatoriaMultiplesNumeros(2, 5, 10, 9, 20, 10 )}`); // 56
+console.log(`Múltiples números: ${ sumatoriaMultiplesNumeros(2)}`); // 2 
+
+/*
+------------ Funciones de callback -----------------
+Es una función (definida, expresada, arrow, anónima) que se pasa a otra función como argumento.
+Se para en el argumento como referencia ( sin paréntesis ).
+
+*/
+
+function saludoALosPokemones(){
+    return "yo te elijo";
+}
+
+/* function saludoSquirtle( nombre ){
+    return "Vamo a calmano "; + nombre;
+}
+
+function eligiendoPokebola( saludo ){
+    console.log("==============================")
+    console.log("Hola, estás en la liga pokemon");
+    console.log("Elige a tu mejor pokemon");
+    console.log( saludo(nombre) );
+}
+
+//eligiendoPokebola(); // saludo is not a function  (esta mal)
+
+//forma correcta de usar la funcion
+eligiendoPokebola( saludoALosPokemones ); //"yo te elijo"
+eligiendoPokebola( saludoSquirtle); // "Vamo a calmano"
+eligiendoPokebola( function(){return "Pika Pika Chuu"});
+//eligiendoPokebola( function(nombre){return "quiiiii soy " + nombre}, "Cubone");
+ */
+//const eligiendoPokebola = (nombre) => "quiiiii soy " + nombre ("Cubone");// esta mal
+//eligiendoPokebola( (nombre) => `quiiiii soy ${nombre}` ,  "Charizard");
+
+/*
+  Ejercicio 4
+  Crear un programa que itere sobre dos arreglos;
+  si hay cursos en común, imprimirlos en la consola.
+
+ student1Courses = ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+
+  salida: "Cursos en común: Programming, Music"
+*/
+
+//--------------- Resolviendo con ciclos anidados ----------------------
+const student1Courses = ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+const student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+
+
+function cursosEnComun( student1Courses,  student2Courses  ){
+    const commonCourses = []; // guardar los cursos en comun
+
+    for (let i = 0; i < student1Courses.length; i++) { // ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+        for (let j = 0; j < student2Courses.length; j++) { // ["Geography", "Spanish", "Programming", "Music"]
+            console.log(`student1Courses[i] === student2Courses[j] : ${student1Courses[i] === student2Courses[j]}`)
+            if ( student1Courses[i] === student2Courses[j] ){
+                commonCourses.push(student1Courses[i]);
+            }
+    
+        }
+        
+    }
+
+    return `Cursos en común ${commonCourses}`
+}
+cursosEnComun( student1Courses, student2Courses );
+
